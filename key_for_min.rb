e4e10c604a8e9d5ded3615c70e9a5d9f1b760752
Nathan Bell <nathanbell09@gmail.com>
2018-11-09 15:24:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,14 +8,15 @@ def key_for_min_value(name_hash)
     min_value = 0
     min_value_key = ""
     hash_values = name_hash.collect { |key, value| value }
-    name_hash.each { |key, value|
-      hash_values.each { |h_value|
+    name_hash.each do |key, value|
+      hash_values.each do |h_value|
+        puts h_value
         if value < h_value
           min_value = value
           min_value_key = key
         end
-      }
-    }
+      end
+    end
     min_value_key
   end
 end
