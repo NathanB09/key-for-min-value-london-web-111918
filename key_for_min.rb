a80495a9cf466b0797f3ffe012fa2f0472b8c8bf
Nathan Bell <nathanbell09@gmail.com>
2018-11-09 15:14:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,17 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   else
+    min_value = 0
     hash_values = name_hash.collect { |key, value| value }
-    name_hash.each
+    name_hash.each { |key, value|
+      hash_values.each { |h_value|
+        if value < h_value
+          min_value = value
+        end 
+      }
+      if min_value == value
+        return key
+      end 
+    }
   end
 end
